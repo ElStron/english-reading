fdf7ba4bed94696f568013e2f7f49380985aea9d
ElStron <luisstron45@gmail.com>
2025-02-06 14:54:10 -0600

    Update ImagesLoaded message to include BooksList for enhanced data handling

--- src/messages.rs
+++ src/messages.rs
@@ -1,10 +1,10 @@
 use iced::Theme;
-
+use crate::ui::books::BooksList;
 
 #[derive(Debug, Clone)]
 pub enum Message {
     NavigateTo(&'static str),
     ExplainToggled(bool),
     ThemeSelected(Theme),
-    ImagesLoaded(),
+    ImagesLoaded( BooksList ),
 }
