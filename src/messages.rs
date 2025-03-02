0c7e38d42fd4b0af26fb81dbe7b36e326cc8f167
ElStron <luisstron45@gmail.com>
2025-03-01 19:19:35 -0600

    Add new message variants for current book handling and update image loading structure

--- src/messages.rs
+++ src/messages.rs
@@ -1,10 +1,12 @@
+use crate::ui::{books::BooksList, books_details::BookDetails};
 use iced::Theme;
-use crate::ui::books::BooksList;
 
 #[derive(Debug, Clone)]
 pub enum Message {
     NavigateTo(&'static str),
     ExplainToggled(bool),
     ThemeSelected(Theme),
-    ImagesLoaded( BooksList ),
+    ImagesLoaded(BooksList),
+    SetCurrentBook(i32),
+    CurrentBookLoaded(BookDetails),
 }
