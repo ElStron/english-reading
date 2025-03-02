cf683ddf5280e78355ecd5de30b7ef937ce672c5
ElStron <luisstron45@gmail.com>
2025-03-01 19:20:01 -0600

    Refactor books list view to improve loading state handling and update book selection logic

--- src/ui/books.rs
+++ src/ui/books.rs
@@ -1,116 +1,118 @@
+use crate::{Layout, Message};
 use iced::futures::future::join_all;
-use iced::widget::{
-    button, column, container, image, row, scrollable, text, center
-};
-use iced::{
-    Center, Element, Fill
-};
-use crate::{Message, Layout};
+use iced::widget::{button, center, column, container, image, row, scrollable, text};
+use iced::{Center, Element, Fill};
 
-pub fn books_list_view<'a>(layout: &Layout) -> Element<'static, Message>  {
-
+pub fn books_list_view<'a>(layout: &Layout) -> Element<'static, Message> {
     let books_mapped = match layout.loading_books {
-        true => (0..30).map(|_| {
-            //
-            column![
-                center("Loading").style(container::rounded_box),
-            ].width(200).height(300).into()
-        }).collect::<Vec<Element<'_, Message>>>(),
-        false => layout.books_list.books.iter().map(|book| {
-            let image_widget = match &book.handle_imagen {
-                Some(handle) => handle.clone(),
-                None => image::Handle::from_path("./45.png"),
-            };
-            
-            let button = button(
-                column![image(image_widget).width(200),
-                text(book.title.clone()).size(12), 
-            ]
-            .max_width(120)
-            ).on_press(Message::NavigateTo("Reader"));
+        true => (0..30)
+            .map(|_| {
+                //
+                column![center("Loading").style(container::rounded_box),]
+                    .width(200)
+                    .height(300)
+                    .into()
+            })
+            .collect::<Vec<Element<'_, Message>>>(),
+        false => layout
+            .books_list
+            .books
+            .iter()
+            .map(|book| {
+                let image_widget = match &book.handle_imagen {
+                    Some(handle) => handle.clone(),
+                    None => image::Handle::from_path("./45.png"),
+                };
 
-            button.into()
-        }).collect::<Vec<Element<'_, Message>>>()
+                let button = button(
+                    column![
+                        image(image_widget).width(200),
+                        text(book.title.clone()).size(12),
+                    ]
+                    .max_width(120),
+                )
+                .on_press(Message::SetCurrentBook(book.id.clone()));
+
+                button.into()
+            })
+            .collect::<Vec<Element<'_, Message>>>(),
     };
 
     let content = container(
         scrollable(
-            column![
-                row(books_mapped)
-                .spacing(15)
-                .wrap(),
-            ]
-            .spacing(40)
-            .align_x(Center)
-            .width(Fill),
+            column![row(books_mapped).spacing(15).wrap(),]
+                .spacing(40)
+                .align_x(Center)
+                .width(Fill),
         )
         .height(Fill),
-    ).width(Fill);
+    )
+    .width(Fill);
     content.into()
 }
 
-#[derive(Default,Debug, Clone, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub struct BooksList {
     books: Vec<Book>,
 }
 
 #[derive(PartialEq, Clone, Eq, Debug)]
 pub struct Book {
+    id: i32,
     title: String,
     imagen: String,
     description: String,
     pub handle_imagen: Option<image::Handle>,
 }
 
-#[derive(PartialEq, Clone, Eq, Debug,serde::Deserialize)]
+#[derive(PartialEq, Clone, Eq, Debug, serde::Deserialize)]
 struct ApiBook {
+    id: i32,
     title: String,
     url: String,
 }
 
 impl BooksList {
     pub fn new() -> Self {
-        Self {
-            books: vec![]
-        }
+        Self { books: vec![] }
     }
 
     pub fn get_books(&self) -> Vec<Book> {
-        
         self.books.clone()
     }
 
     pub async fn fetch_books(&self) -> Result<Vec<Book>, reqwest::Error> {
         let url = "https://jsonplaceholder.typicode.com/albums/1/photos";
-    
+
         #[cfg(not(target_arch = "wasm32"))]
         {
             let books: Vec<ApiBook> = reqwest::get(url).await?.json().await?;
-            let books_with_images = join_all(
-                books.into_iter().map(|book| async {
-                let handle_imagen = self
-                    .fetch_image("https://picsum.photos/200/300").await; 
+            let books_with_images = join_all(books.into_iter().map(|book| {
+                let book = book.clone();
+                async move {
+                    let handle_imagen = self.fetch_image("https://picsum.photos/200/300").await;
 
-                let imagen = match handle_imagen {
-                    Ok(handle) => handle,
-                    Err(_) => image::Handle::from_path("./45.png"),
-                };
-                Book {
-                    title: book.title.clone(),
-                    imagen: "https://picsum.photos/200/300".to_string(),
-                    description: book.title,
-                    handle_imagen: Some(imagen),
+                    let imagen = match handle_imagen {
+                        Ok(handle) => handle,
+                        Err(_) => image::Handle::from_path("./45.png"),
+                    };
+                    Book {
+                        id: book.id,
+                        title: book.title.clone(),
+                        imagen: "https://picsum.photos/200/300".to_string(),
+                        description: book.title,
+                        handle_imagen: Some(imagen),
+                    }
                 }
-            })).await;
+            }))
+            .await;
             Ok(books_with_images)
         }
         #[cfg(target_arch = "wasm32")]
         Ok(vec![])
-
-    } 
+    }
 
     pub async fn fetch_images(&mut self) {
-
         match self.fetch_books().await {
             Ok(books_with_images) => {
                 self.books = books_with_images;
@@ -122,19 +124,16 @@ impl BooksList {
     }
 
     async fn fetch_image(&self, id: &str) -> Result<image::Handle, reqwest::Error> {
-        let url = format!(
-            "{id}",
-        );
-    
+        let url = format!("{id}",);
+
         #[cfg(not(target_arch = "wasm32"))]
         {
             let bytes = reqwest::get(&url).await?.bytes().await?;
-    
+
             Ok(image::Handle::from_bytes(bytes))
         }
-    
+
         #[cfg(target_arch = "wasm32")]
         Ok(image::Handle::from_path("./45.png"))
     }
 }
-
